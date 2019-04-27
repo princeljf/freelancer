bd7313086fdca31f6f56a970be48c592a1fc774b
lhhformac <384234884@qq.com>
2019-04-27 21:51:21 +0800

    lhh_add

---
+++ python/getData.py
@@ -0,0 +1,8 @@
+import getDataFn as getData
+
+df = getData.index_basic()
+# df = getData.stock_basic(path='/Users/liujunfan/princeljf/node_lhh/lhh/src/datas/stock_basic/', orient='records')
+# df = getData.code('000001')
+print( df )
+
+
